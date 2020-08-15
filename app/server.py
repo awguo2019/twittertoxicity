2acb7afaa9059032bb89f235b86e662716520d1b
Alan Guo <awguo2019@gmail.com>
2020-08-14 23:53:35 -0400

    Update server.py
    adding in twitter data

--- app/server.py
+++ app/server.py
@@ -8,6 +8,7 @@ from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
+import tweepy
 
 export_file_url = 'https://www.googleapis.com/drive/v3/files/1v4rpPSkHkOaYDM1RvExukQMriOqXNbhN?alt=media&key=AIzaSyAZxRmGvTn8CCz084JSla4u5Gei8Af3_VM'
 export_file_name = 'export.pkl'
@@ -31,6 +32,15 @@ async def download_file(url, dest):
 async def setup_learner():
     await download_file(export_file_url, path / export_file_name)
     try:
+        
+        accesstoken = '1291150581256597504-ZU1jkJf1ysWub7T3Q3lV3B8DVswQex'
+        accesstoken_secret = 'JTkHZqop5vjoyg6N7WYmV7OLsfoMKl8aS2TEoZr4szzvm'
+        api_key = 'j57Hnlx82JuzmkoWfDx1nytq1'
+        api_key_secret = '8t9saBhTE16jZ7Sol3koEjykyiSnbWPu3QXf45g3wG4zAkx2q0'
+        auth = tweepy.OAuthHandler(api_key, api_key_secret)
+        auth.set_access_token(accesstoken, accesstoken_secret)
+        api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+        
         learn = load_learner(path, export_file_name)
         return learn
     except RuntimeError as e:
@@ -62,12 +72,36 @@ async def analyze(request):
     # img_bytes = await (data['file'].read())
     # took out img_bytes
     # img = open_image(BytesIO(img_bytes))
+    
+    
+    
     img = data["textField"]
     print("data['textField']", data["textField"])
     print("img:", img)
+    
+    tweets = tweepy.Cursor(api.search,
+                       q=img,
+                       lang="en").items(500)
+    tweetlist = [];
+    for tweet in tweets:
+        tweetlist.append(tweet.text);
+    pred_list = TextList(tweetlist)
+    
+    preds, target = learn.get_preds(ds_type=pred_list, ordered=True) #check this with ds_type !
+    labels = np.argmax(preds, 1)
+    out=labels.numpy()
+    
+    output = pd.DataFrame(data = out)
+    xzzz = list(pred_list)
+    df = pd.DataFrame(xzzz)
+    df["toxic"] = output
+    
+    percent = sum(out)/500
+
     #prediction = learn.predict(img)
     #print("prediction:", prediction)
-    return JSONResponse({'result': img})
+    
+    return JSONResponse({'result': percent})
 
 
 if __name__ == '__main__':
