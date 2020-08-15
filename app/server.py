1295a95beda245f102d96bd6eaf9e6a388682e04
Alan Guo <awguo2019@gmail.com>
2020-08-14 22:08:25 -0400

    Update server.py
    making changes to server to output textbox text

--- app/server.py
+++ app/server.py
@@ -56,13 +56,13 @@ async def homepage(request):
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
-    img_data = await request.form()
-    img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    str_data = await request.form()
+    str_bytes = await (img_data['file'].read())
+#    img = open_image(BytesIO(img_bytes))
+#    prediction = learn.predict(img)[0]
+    return JSONResponse({'result': str_bytes})
 
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
-        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
+        uvicorn.run(app=app, host='0.0.0.0', port=605413, log_level="info")
