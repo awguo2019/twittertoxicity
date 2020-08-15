ee3b47fd7c985f36a659a640d61638b375a1a9c4
Alan Guo <awguo2019@gmail.com>
2020-08-14 21:35:43 -0400

    Update server.py
    putting in nlp model

--- app/server.py
+++ app/server.py
@@ -2,17 +2,16 @@ import aiohttp
 import asyncio
 import uvicorn
 from fastai import *
-from fastai.vision import *
+from fastai.text import *
 from io import BytesIO
 from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1btPHzF0-yMvm4UADP5fBP3l7S8raWqZS'
+export_file_name = 'nlp_model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
