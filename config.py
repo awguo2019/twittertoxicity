687aa7cf0a8c23c81ea56ec0b8d39ef63935a4c4
Alan Guo <awguo2019@gmail.com>
2020-08-15 01:19:17 -0400

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-api_key = "XmgCBVVleESTzLBbesBWNFWRr"
-api_secret = "ikOmADnjBm9W8ujcAMRN5Uzcg3tBwsEtzS2HhtL3NxGiUIEyPn"
-access_token = "1291150581256597504-z0aRzKzhAzMFy5FEeyQaf33VDVYiXZ"
-token_secret = "HbZOF0xo3PKio94f3oBNUTSMU09znv4N2oQUGhove9OS6"
